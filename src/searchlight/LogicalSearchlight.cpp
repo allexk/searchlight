c155c30fe1c65f38db2f02f1f662032866c9be36
Alexander Kalinin <allex2001@gmail.com>
2014-05-19 01:30:03 -0400

    sl: Searchlight SciDb operator

---
+++ src/searchlight/LogicalSearchlight.cpp
@@ -0,0 +1,111 @@
+/* Copyright 2014, Brown University, Providence, RI.
+ *
+ *                         All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose other than its incorporation into a
+ * commercial product is hereby granted without fee, provided that the
+ * above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of Brown University not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.
+ *
+ * BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ANY
+ * PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY BE LIABLE FOR
+ * ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * @file LogicalSearchlight.cpp
+ * The implementation of the logical part of the searchlight operator.
+ *
+ * @brief The operator: searchlight().
+ *
+ * @par Synopsis:
+ *   <br/>
+ *   searchlight(input_array, sample_array, 'library:function:task_params').
+ *
+ * @par Summary:
+ *   <br/>
+ *   This operator runs a task via Searchlight. It finds the specified function
+ *   in the specified library, which is a DLL. The function itself takes
+ *   the Searechlight instance and a string of parameters, which it supposed
+ *   to parse by itself.
+ *   <br/>
+ *   The operator outputs an array of results with indexes [0, *]. The right
+ *   boundary is undefined since the number of results is unknown beforehand.
+ *   The array is output in the stream mode, which allows it to be processed
+ *   online.
+ *
+ * @par Input:
+ *   <br> input_array -- any SciDB array
+ *   <br> sample_array -- a sample array for the specified array. The array
+ *      must correspond to the following format: dimensions[chunk: 0, *;
+ *      sampled_attribute: original attribute index], attributes:[min:double,
+ *      max: double, count: uint64, sum: double].
+ *   <br> 'library:function:task_params'  -- the name of the DLL where the
+ *      tasks come from. The name of the task (function), which should take
+ *      Searchlight & and string as parameters. The parameters, which are
+ *      parsed by the function itself.
+ *
+ * @par Output array:
+ *   <br> <
+ *   <br>   assignment :string
+ *   <br> >
+ *   <br> [
+ *   <br>   num = 0:*,1 0]
+ *   <br> ]
+ *
+ * @author Alexander Kalinin
+ */
+
+#include <query/Operator.h>
+
+#include "searchlight_task.h"
+
+namespace scidb {
+
+/**
+ * The Logical Operator object for searchlight.
+ */
+class LogicalSearchlight : public LogicalOperator {
+public:
+    /**
+     * Searchlight operator constructor.
+     *
+     * @param logicalName used internally by scidb
+     * @param alias used internally by scidb
+     */
+    LogicalSearchlight(const std::string& logicalName,
+            const std::string& alias) :
+        LogicalOperator(logicalName, alias) {
+
+        ADD_PARAM_INPUT() // array
+        ADD_PARAM_INPUT() // sample
+        ADD_PARAM_CONSTANT(TID_STRING) // params
+    }
+
+    /**
+     * Returns the descriptor of the result.
+     *
+     * @param schemas all of the schemas of the input arrays
+     *  (if the operator accepts any)
+     * @param query the query context
+     * @return the schema of the output
+     */
+    ArrayDesc inferSchema(std::vector<ArrayDesc> schemas,
+            boost::shared_ptr<Query> query) {
+
+        return searchlight::SearchlightResultsArray::GetArrayDesc();
+    }
+};
+
+// Register the operator
+REGISTER_LOGICAL_OPERATOR_FACTORY(LogicalSearchlight, "searchlight");
+
+} /* namespace scidb */
---
+++ src/searchlight/PhysicalSearchlight.cpp
@@ -0,0 +1,110 @@
+/* Copyright 2014, Brown University, Providence, RI.
+ *
+ *                         All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose other than its incorporation into a
+ * commercial product is hereby granted without fee, provided that the
+ * above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of Brown University not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.
+ *
+ * BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR ANY
+ * PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY BE LIABLE FOR
+ * ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * @file PhysicalSearchlight.cpp
+ * The implementation of the physical part of the searchlight operator.
+ *
+ * @see LogicalSearchlight.cpp for the description of the operator itself.
+ *
+ * @author Alexander Kalinin
+ */
+
+#include <query/Operator.h>
+
+#include "searchlight_task.h"
+
+namespace scidb
+{
+
+/**
+ * Implements the physical part of the operator. It instantiates a new
+ * SearchlightTask, which calls an appropriate library function. It also
+ * returns the resulting array, which is lazily filled by calls from the
+ * upstream operators.
+ */
+class PhysicalSearchlight : public PhysicalOperator
+{
+public:
+    /**
+     * Constructs a new physical part of the Searchlight operator.
+     *
+     * @param logicalName the name of the logical part
+     * @param physicalName the name of the physical part
+     * @param parameters params (parsed in the logical part)
+     * @param schema the schema of the result
+     */
+    PhysicalSearchlight(const std::string &logicalName,
+            const std::string &physicalName, const Parameters &parameters,
+            const ArrayDesc &schema) :
+        PhysicalOperator(logicalName, physicalName, parameters, schema) {}
+
+    /**
+     * Execute the operator and return the output array. Note that we do not
+     * run the actual search until the first chunk of the array is requested.
+     *
+     * @param inputArrays the input array arguments. Data and sample.
+     * @param query the query context
+     * @return the output array object
+     */
+    boost::shared_ptr<Array> execute(
+            std::vector<boost::shared_ptr<Array>> &inputArrays,
+            boost::shared_ptr<Query> query) {
+
+        // tokenize params
+        std::string lib_func_param =
+                ((boost::shared_ptr<OperatorParamPhysicalExpression> &)
+                        _parameters[0])->getExpression()->
+                        evaluate().getString();
+        typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+        tokenizer tokens(lib_func_param, boost::char_separator<char>(":|,", 0,
+                boost::keep_empty_tokens));
+        searchlight::StringVector str_tokens;
+        for (tokenizer::iterator it = tokens.begin(); it != tokens.end();
+                ++it) {
+            str_tokens.push_back(*it);
+        }
+
+        // FIXME: should probably check this in the logical part
+        if (str_tokens.size() != 3 || str_tokens[0].empty()
+                ||str_tokens[1].empty()) {
+            throw SYSTEM_EXCEPTION(SCIDB_SE_OPERATOR,
+                    SCIDB_LE_ILLEGAL_OPERATION) <<
+                            "Illegal searchlight parameters!";
+        }
+
+        searchlight::SearchlightTaskPtr sl_task =
+                boost::make_shared<searchlight::SearchlightTask>(str_tokens[0],
+                        str_tokens[1], str_tokens[2]);
+        sl_task->GetSearchlight().RegisterArray(inputArrays[0], inputArrays[1]);
+        return ArrayPtr(new searchlight::SearchlightResultsArray(sl_task,
+                _schema));
+    }
+};
+
+/*
+ * Register the operator.
+ */
+REGISTER_PHYSICAL_OPERATOR_FACTORY(PhysicalSearchlight, "searchlight",
+        "PhysicalSearchlight");
+
+} /* namespace scidb */
