33b144e56b171976c686f82b04d1d741e8d054b5
Alexander Kalinin <allex2001@gmail.com>
2014-05-21 02:52:04 -0400

    cmake: building system complete

--- cmake/Modules/FindOrTools.cmake
+++ cmake/Modules/FindOrTools.cmake
@@ -19,16 +19,18 @@ message(STATUS "Using ${ORTOOLS_DIR} as the or-tools hint directory...")
 
 # find includes and libraries
 find_path(ORTOOLS_INCLUDE_DIR constraint_solver/constraint_solver.h HINTS ${ORTOOLS_DIR}/src DOC "or-tools include directory")
+find_path(ORTOOLS_INCLUDE_GEN_DIR constraint_solver/model.pb.h HINTS ${ORTOOLS_DIR}/src/gen DOC "or-tools generated files include directory")
 find_library(ORTOOLS_CP_LIBRARY constraint_solver HINTS ${ORTOOLS_DIR}/lib DOC "or-tools constraint solver library")
 find_library(ORTOOLS_LP_LIBRARY linear_solver HINTS ${ORTOOLS_DIR}/lib DOC "or-tools linear solver library")
 find_library(ORTOOLS_BASE_LIBRARY base HINTS ${ORTOOLS_DIR}/lib DOC "or-tools base library")
 find_library(ORTOOLS_UTIL_LIBRARY util HINTS ${ORTOOLS_DIR}/lib DOC "or-tools util library")
-mark_as_advanced(ORTOOLS_LIBRARY ORTOOLS_INCLUDE_DIR ORTOOLS_BASE_LIBRARY ORTOOLS_UTIL_LIBRARY ORTOOLS_LP_LIBRARY)
+mark_as_advanced(ORTOOLS_LIBRARY ORTOOLS_INCLUDE_DIR ORTOOLS_INCLUDE_GEN_DIR ORTOOLS_BASE_LIBRARY ORTOOLS_UTIL_LIBRARY ORTOOLS_LP_LIBRARY)
 
 # standard handling
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OrTools DEFAULT_MSG ORTOOLS_CP_LIBRARY ORTOOLS_LP_LIBRARY ORTOOLS_BASE_LIBRARY ORTOOLS_UTIL_LIBRARY ORTOOLS_INCLUDE_DIR)
+find_package_handle_standard_args(OrTools DEFAULT_MSG ORTOOLS_CP_LIBRARY ORTOOLS_LP_LIBRARY ORTOOLS_BASE_LIBRARY
+    ORTOOLS_UTIL_LIBRARY ORTOOLS_INCLUDE_DIR ORTOOLS_INCLUDE_GEN_DIR)
 
 # set export vars
 set(ORTOOLS_LIBRARIES ${ORTOOLS_CP_LIBRARY} ${ORTOOLS_BASE_LIBRARY} ${ORTOOLS_UTIL_LIBRARY} ${ORTOOLS_LP_LIBRARY})
-set(ORTOOLS_INCLUDE_DIRS ${ORTOOLS_INCLUDE_DIR})
+set(ORTOOLS_INCLUDE_DIRS ${ORTOOLS_INCLUDE_DIR} ${ORTOOLS_INCLUDE_GEN_DIR})
---
+++ cmake/Modules/FindSciDB.cmake
@@ -0,0 +1,29 @@
+#
+# Try to find SciDb sources and determine proper includes
+#
+# Once done this will define
+#  SCIDB_FOUND        - TRUE if SciDb is found
+#  SCIDB_SOURCE_DIR   - Where to find SciDb source directory
+#  SCIDB_INCLUDE_DIRS - What directories to use for SciDb includes
+#
+# You can define SCIDB_DIR to hint the scidb's location.
+#
+
+# Establish a hint
+if (NOT SCIDB_DIR)
+    message(STATUS "You can use SCIDB_DIR variable to specify the SciDb sources directory. Using the defaut hint.")
+    # assume we have a common source directory for projects
+    set(SCIDB_DIR ${CMAKE_SOURCE_DIR}/../scidb-13.6)
+endif()
+message(STATUS "Using ${SCIDB_DIR} as the SciDb hint directory...")
+
+# find includes and libraries
+find_path(SCIDB_SOURCE_DIR include/SciDBAPI.h HINTS ${SCIDB_DIR} DOC "SciDb source directory")
+mark_as_advanced(SCIDB_SOURCE_DIR)
+
+# standard handling
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SciDb DEFAULT_MSG SCIDB_SOURCE_DIR)
+
+# set export vars
+set(SCIDB_INCLUDE_DIRS ${SCIDB_SOURCE_DIR}/include ${SCIDB_SOURCE_DIR}/src)
