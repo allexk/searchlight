bc2dac90f05ba75047da9c0aace48f1da556f127
Alexander Kalinin <allex2001@gmail.com>
2014-11-11 15:52:58 -0500

    scidb_query: multiple queries and intermediate timings

--- utils/query_scidb.py
+++ utils/query_scidb.py
@@ -21,10 +21,18 @@ def handle_exception(inst, exit, op=None):
         sys.exit(2)
 
 def get_query(query_file):
-    "Reads the query from the specified file"
+    """
+    Reads queries from the specified file.
+    
+    It is assumed that each line contains exactly one query
+    """
+    queries = []
     with open(query_file, 'r') as q:
-        query = q.read().strip()
-    return query
+        for l in q:
+            query = l.strip()
+            if query:
+                queries.append(query)
+    return queries
 
 def print_stats(times):
     "Print times statistics about the query execution"
@@ -43,42 +51,40 @@ def print_stats(times):
     else:
         print("No results found --- no duration stats!")
 
-def execute_query(query):
-    "Executes the query and times start, end and result times"
-    times = []
+def execute_query(queries, times):
+    "Executes the queries and times the start, end and result times"
     try:
         db = scidb.connect("localhost", 1239)
     except Exception, inst:
         # terminate
         handle_exception(inst, True, op="connecting")
 
-    print("Executing:", query)
-    try:
-        times.append(datetime.datetime.now())
-        result = db.executeQuery(query, "afl")
-    except Exception, inst:
-        # terminate
-        handle_exception(inst, True, op="Performing select")
+    for query in queries:
+        print("Executing:", query)
+        try:
+            result = db.executeQuery(query, "afl")
+        except Exception, inst:
+            # terminate
+            handle_exception(inst, True, op="Performing select")
 
-    # More precisely, we measure chunk times, since all result in a chunk
-    # can be output with minimal delays. This should be enough to measure
-    # interactive performance.
-    desc = result.array.getArrayDesc()
-    dims = desc.getDimensions()
-    attrs = desc.getAttributes()
+        # More precisely, we measure chunk times, since all result in a chunk
+        # can be output with minimal delays. This should be enough to measure
+        # interactive performance.
+        desc = result.array.getArrayDesc()
+        dims = desc.getDimensions()
+        attrs = desc.getAttributes()
 
-    # print result's schema
-    print("Dimensions:")
-    for i in range(dims.size()):
-        print("\tDimension[%d] = %d:%d,%d,%d\n" % \
-            (i, dims[i].getStartMin(), dims[i].getEndMax(),
-             dims[i].getChunkInterval(), dims[i].getChunkOverlap()))
-    print("Attributes:")
-    for i in range(attrs.size()):
-        print("\tAttribute %d %s %s" % \
-            (attrs[i].getId(), attrs[i].getName(), attrs[i].getType()))
+        # print result's schema
+        print("Dimensions:")
+        for i in range(dims.size()):
+            print("\tDimension[%d] = %d:%d,%d,%d\n" % \
+                (i, dims[i].getStartMin(), dims[i].getEndMax(),
+                 dims[i].getChunkInterval(), dims[i].getChunkOverlap()))
+        print("Attributes:")
+        for i in range(attrs.size()):
+            print("\tAttribute %d %s %s" % \
+                (attrs[i].getId(), attrs[i].getName(), attrs[i].getType()))
 
-    try:
         # attribute partitioning: an iterator per attribute
         iters = []
         for i in range(attrs.size()):
@@ -90,31 +96,44 @@ def execute_query(query):
         while not iters[0].end():
             # get and time another portion of the result
             chunks = []
+            res_count = 0
             for i in range(len(iters)):
-                chunks.append(iters[i].getChunk())
-            times.append(datetime.datetime.now())
+                chunk = iters[i].getChunk()
+                res_count += chunk.count()
+                chunks.append(chunk)
+
+            # ignore empty result chunks (e.g., happens for filter())
+            if res_count > 0:
+                times.append(datetime.datetime.now())
 
             for i in range(len(iters)):
                 iters[i].increment_to_next()
-    except KeyboardInterrupt:
-        # on Control-C just exit
-        print('Interrupted by Control-C')
+
+        try:
+            db.completeQuery(result.queryID)
+        except:
+            handle_exception(inst, True, op="completing query")
 
     try:
-        db.completeQuery(result.queryID)
         db.disconnect()  # Disconnect from the SciDB server.
     except:
         handle_exception(inst, False, op="disconnecting")
-
-    times.append(datetime.datetime.now())
-    return times
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python query_scidb.py <query_file>")
         sys.exit(1)
 
-    query = get_query(sys.argv[1])
-    times = execute_query(query)
+    queries = get_query(sys.argv[1])
+    times = []
+    try:
+        times.append(datetime.datetime.now())
+        execute_query(queries, times)
+    except:
+        # on Control-C or some error just exit
+        print('Interrupted for an unknown reason!')
+    finally:
+        times.append(datetime.datetime.now())
+
     print_stats(times)
     sys.exit(0)
