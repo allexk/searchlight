cb2e42176e980ee6412e3a6c66ff74c2ad538321
Alexander Kalinin <allex2001@gmail.com>
2015-12-05 17:18:21 -0500

    cmake: move back to dynamic linking due to errors

--- cmake/Modules/FindOrTools.cmake
+++ cmake/Modules/FindOrTools.cmake
@@ -17,14 +17,22 @@ if (NOT ORTOOLS_DIR)
 endif()
 message(STATUS "Using ${ORTOOLS_DIR} as the or-tools hint directory...")
 
-# find includes and libraries (prefer static)
+# find includes and libraries
+#
+# NOTE: we'd like to go static here. Unfortunately, we cannot do it reliably.
+# We have several dynamic libs, like the main UDO and the task ones. Each
+# might link with the or-tools static libs. Or-tools uses gflags options,
+# so duplicating or-tools object files across multiple libs might result
+# in gflags errors, like "something wrong with flag F". The same error might
+# also occur if we open/close the same dynamic lib during the execution, and
+# we do it with the task libs. Thus, WE Do THE DYNAMIC LINKING HERE.
 find_path(ORTOOLS_INCLUDE_DIR constraint_solver/constraint_solver.h HINTS ${ORTOOLS_DIR}/src DOC "or-tools include directory")
 find_path(ORTOOLS_INCLUDE_GEN_DIR constraint_solver/model.pb.h HINTS ${ORTOOLS_DIR}/src/gen DOC "or-tools generated files include directory")
-find_library(ORTOOLS_CP_LIBRARY libconstraint_solver.a constraint_solver HINTS ${ORTOOLS_DIR}/lib DOC "or-tools constraint solver library")
-find_library(ORTOOLS_LP_LIBRARY liblinear_solver.a linear_solver HINTS ${ORTOOLS_DIR}/lib DOC "or-tools linear solver library")
-find_library(ORTOOLS_BASE_LIBRARY libbase.a base HINTS ${ORTOOLS_DIR}/lib DOC "or-tools base library")
-find_library(ORTOOLS_UTIL_LIBRARY libutil.a util HINTS ${ORTOOLS_DIR}/lib DOC "or-tools util library")
-mark_as_advanced(ORTOOLS_LIBRARY ORTOOLS_INCLUDE_DIR ORTOOLS_INCLUDE_GEN_DIR ORTOOLS_BASE_LIBRARY ORTOOLS_UTIL_LIBRARY ORTOOLS_LP_LIBRARY)
+find_library(ORTOOLS_CP_LIBRARY constraint_solver HINTS ${ORTOOLS_DIR}/lib DOC "or-tools constraint solver library")
+find_library(ORTOOLS_LP_LIBRARY linear_solver HINTS ${ORTOOLS_DIR}/lib DOC "or-tools linear solver library")
+find_library(ORTOOLS_BASE_LIBRARY base HINTS ${ORTOOLS_DIR}/lib DOC "or-tools base library")
+find_library(ORTOOLS_UTIL_LIBRARY util HINTS ${ORTOOLS_DIR}/lib DOC "or-tools util library")
+mark_as_advanced(ORTOOLS_LIBRARY ORTOOLS_INCLUDE_DIR ORTOOLS_INCLUDE_GEN_DIR ORTOOLS_BASE_LIBRARY ORTOOLS_UTIL_LIBRARY ORTOOLS_LP_LIBRARY ORTOOLS_CP_LIBRARY)
 
 # standard handling
 include(FindPackageHandleStandardArgs)
